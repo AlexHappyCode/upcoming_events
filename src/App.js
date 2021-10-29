08707d2c961f1d1e5cc1fc22017a807284b568fe
Kiran <Kiran2x15@gmail.com>
2021-10-29 00:10:50 -0700

    Fixed app.js to show all content made so far

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button } from 'react-bootstrap';
+import Header from './components/Header';
+import Content from './components/Content';
 
 
 
 
 function App() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://source.unsplash.com/user/erondu/600x400" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some Custom text one can write here
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <React.Fragment>
+    <Header />
+    <Content />
+    </React.Fragment>
   );
 }
 

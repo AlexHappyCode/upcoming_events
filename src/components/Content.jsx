c063805e0639f837c86dd4eed02983a9bdedc35d
Kiran <Kiran2x15@gmail.com>
2021-10-31 04:41:40 -0700

    4am work

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, CardGroup, Card, Row, Col, Button } from 'react-bootstrap';
+import { Container, CardGroup, Card, Row, Col, Button, } from 'react-bootstrap';
 
 
 export default function Content() {
@@ -11,20 +11,12 @@ return(
       {/* Where cards are */}
       <CardGroup style={{flex: 1}} >
         <Card  >
-        <Card.Header as="h5" style={headerStyle}>Next Event</Card.Header>
+        <Card.Header as="h5" style={headerStyle}>Happening Soon</Card.Header>
         {/* Row is card row */}
           <Row>
-            <Col>
-            {/* SVG This is where the image goes */}
-            <svg
-            className="bd-placeholder-img"
-            width="100%"
-            height="250"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Placeholder: Image"
-            preserveAspectRatio="xMidYMid slice"
-            role="img"
-          />
+            <Col style={imageStyle}>
+            {/*  This is where the image goes */}
+            <Card.Img src={"https://www.google.com/logos/doodles/2021/halloween-2021-6753651837109122-law.gif"} fluid={true} alt="Card image" />
             </Col>
             <Col>
             {/* Card body has card title and card text inside of it */}
@@ -45,60 +37,27 @@ return(
       </CardGroup>
     </Container>
 
-    {/* Upcoming Events Container */}
+    {/* Upcoming Events Container*/}
     <Container fluid style={containerStyle}>
+      {/* Where cards are */}
       <CardGroup style={{flex: 1}} >
-        {/* Multiple cards in card groups, literally copy and paste */}
-
         <Card  >
-        <Card.Header as="h5" style={headerStyle}>Coming Soon</Card.Header>
-          <Row Style={rowStyle}>
-            <Col>
-            <svg
-            className="bd-placeholder-img"
-            width="100%"
-            height="250"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Placeholder: Image"
-            preserveAspectRatio="xMidYMid slice"
-            role="img"
-          />
+        <Card.Header as="h5" style={headerStyle}>Upcoming Events</Card.Header>
+        {/* Row is card row */}
+          <Row>
+            <Col style={imageStyle}>
+            {/*  This is where the image goes */}
+            <Card.Img src={"https://www.google.com/logos/doodles/2021/halloween-2021-6753651837109122-law.gif"} fluid={true} alt="Card image" />
             </Col>
             <Col>
-            
+            {/* Card body has card title and card text inside of it */}
               <Card.Body>
               <Card.Title as="h1">Blah blah blah</Card.Title>
               <Card.Text as="h4">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos fuga fugit quae quia adipisci consectetur non eaque saepe, quibusdam deleniti delectus recusandae, ipsam sint et ipsum consequatur nisi voluptate obcaecati.
               </Card.Text>
+              {/* Regular button with extra stylings */}
               <div id= 'button'>
-              <Button style={buttonStyle}>Go somewhere</Button>
-              </div>
-              </Card.Body>
-              
-            </Col>
-          </Row>
-          {/* Row 2 */}
-          <Row Style={rowStyle}>
-            <Col>
-            <svg
-            className="bd-placeholder-img"
-            width="100%"
-            height="250"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Placeholder: Image"
-            preserveAspectRatio="xMidYMid slice"
-            role="img"
-          />
-            </Col>
-            <Col>
-            
-              <Card.Body>
-              <Card.Title as="h1">Blah blah blah</Card.Title>
-              <Card.Text as="h4">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos fuga fugit quae quia adipisci consectetur non eaque saepe, quibusdam deleniti delectus recusandae, ipsam sint et ipsum consequatur nisi voluptate obcaecati.
-              </Card.Text>
-              <div>
               <Button style={buttonStyle}>Go somewhere</Button>
               </div>
               </Card.Body>
@@ -120,23 +79,32 @@ return(
 // This is where the styles ares
 const buttonStyle = {
     alignItems: 'center',
-    float: 'right',
+    float: 'left',
     backgroundColor: '#771EF7',
     padding: 10,
     
   };
+  const imageStyle ={
+    alignItems: 'center',
+    float: 'center',
+  }
 const headerStyle={
     marginTop: -1,
-    marginLeft:-2,
-    marginRight:-2,
     marginBottom:-1.5,
 
-    backgroundColor: '#771EF7',
-    color: '#FFFFFF',
+    backgroundColor: '#FFFF',
+    color: '#000000',
 }
 const containerStyle ={
     padding:0,
+    paddingLeft:10,
+    paddingRight:10,
+    shadowColor: 'rgba(0,0,0, .2)',
+    shadowOffset: { height: 0, width: 0 },
+    shadowOpacity: 0, //default is 1
+    shadowRadius: 0
+
 }
 const rowStyle ={
-  border:'2px solid black',
+  // border:'2px solid black',
 }

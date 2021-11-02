1c9e8e4803681ec6f841b3071dd4b2f02a02ff9b
Alexander Pena <arpenacoding@gmail.com>
2021-11-02 14:17:58 -0700

    fixed with prettier

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,31 @@
-import EventCard from './EventCard'
+import EventCard from "./EventCard";
 
-const eventDescription = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos\
+const eventDescription =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos\
                   fuga fugit quae quia adipisci consectetur non eaque saepe,\
                   quibusdam deleniti delectus recusandae, ipsam sint et ipsum\
-                  consequatur nisi teest voluptate obcaecati.'
+                  consequatur nisi teest voluptate obcaecati.";
 
-const eventHeader = 'Coming Soon!'
+const eventHeader = "Coming Soon!";
 
 export default function Content() {
   return (
     <>
-      <EventCard eventHeader={eventHeader} eventTitle="First event" eventDescription={eventDescription}></EventCard>
-      <EventCard eventHeader={eventHeader} eventTitle="Second Event" eventDescription={eventDescription}></EventCard>
-      <EventCard eventHeader={eventHeader} eventTitle="Third Event" eventDescription={eventDescription}></EventCard>
+      <EventCard
+        eventHeader={eventHeader}
+        eventTitle="First event"
+        eventDescription={eventDescription}
+      ></EventCard>
+      <EventCard
+        eventHeader={eventHeader}
+        eventTitle="Second Event"
+        eventDescription={eventDescription}
+      ></EventCard>
+      <EventCard
+        eventHeader={eventHeader}
+        eventTitle="Third Event"
+        eventDescription={eventDescription}
+      ></EventCard>
     </>
   );
 }

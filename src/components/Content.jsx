462d91120002b03c201910748b2ec0fb421e5c71
Alexander Pena <arpenacoding@gmail.com>
2021-11-02 13:57:53 -0700

    added dynamic attributes for eventCard

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,16 @@
 import EventCard from './EventCard'
 
+const eventDescription = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos\
+                  fuga fugit quae quia adipisci consectetur non eaque saepe,\
+                  quibusdam deleniti delectus recusandae, ipsam sint et ipsum\
+                  consequatur nisi teest voluptate obcaecati.'
+
 export default function Content() {
   return (
     <>
-      <EventCard></EventCard>
-      <EventCard></EventCard>
-      <EventCard></EventCard>
+      <EventCard eventTitle="First event" eventDescription={eventDescription}></EventCard>
+      <EventCard eventTitle="Second Event" eventDescription={eventDescription}></EventCard>
+      <EventCard eventTitle="Third Event" eventDescription={eventDescription}></EventCard>
     </>
   );
 }
--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,13 +1,13 @@
 import { Container, CardGroup, Card, Row, Col, Button } from "react-bootstrap";
 
-export default function EventCard() {
+export default function EventCard(props) {
   return (
     <Container>
       <CardGroup>
         <Card>
-          <Card.Header as="h5">Happening Soon</Card.Header>
           <Row>
             <Col>
+              <Card.Header as="h5">Happening Soon</Card.Header>
               <Card.Img
                 src={
                   "https://www.google.com/logos/doodles/2021/halloween-2021-6753651837109122-law.gif"
@@ -18,13 +18,9 @@ export default function EventCard() {
             </Col>
             <Col>
               <Card.Body>
-                <Card.Title as="h1">Blah blah blah</Card.Title>
-                <Card.Text as="h4">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-                  fuga fugit quae quia adipisci consectetur non eaque saepe,
-                  quibusdam deleniti delectus recusandae, ipsam sint et ipsum
-                  consequatur nisi voluptate obcaecati.
-                </Card.Text>
+                <Card.Text>Date here</Card.Text>
+                <Card.Title as="h2">{props.eventTitle}</Card.Title>
+                <Card.Text as="h4">{props.eventDescription}</Card.Text>
                 <div id="button">
                   <Button>Go somewhere</Button>
                 </div>
